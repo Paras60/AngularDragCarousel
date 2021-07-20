bd751a39123afc6d84ab3844ce07796cff6995fc
Paras Choudhary <paras.choudhary@ibm.com>
2021-07-20 14:39:06 +0530

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { Injectable, NgModule } from '@angular/core';
+import { BrowserModule, HammerGestureConfig, HammerModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import * as Hammer from 'hammerjs'
+import { AppComponent } from './app.component';
+@Injectable()
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides = <any> {
+    swipe: { direction: Hammer.DIRECTION_ALL },
+  };
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HammerModule
+  ],
+  providers: [
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: MyHammerConfig,
+    },
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
